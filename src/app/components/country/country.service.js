3af1160dc5ee7bbc2d25894194c61090e2ec66f4
Anton Goncharuk <antonge92@gmail.com>
2016-04-11 13:11:47 +0300

    Implement logic in country service to load selected country profile.

--- src/app/components/country/country.service.js
+++ src/app/components/country/country.service.js
@@ -1,10 +1,46 @@
 export class CountryService {
-  constructor($http) {
+  constructor($http, $q) {
     'ngInject';
+
     this.$http = $http;
+    this.$q = $q;
   }
 
-  loadCountryProfile(countryName) {
-    console.log(countryName);
+  loadCountryProfile(country) {
+    if (!country) return false;
+
+    return this.$http.get('/resources/data/countryprofiledata.json')
+      .then(
+        (response) => {
+          var selectedProfile = null;
+          var keepGoing = true;
+
+          angular.forEach(response.data, (value, key) => {
+            if (keepGoing) {
+              if (key === country.code) { selectedProfile = value; keepGoing = false; }
+            }
+          });
+
+          return selectedProfile;
+        },
+        // @todo: error message
+        (response) => response);
   }
+
+  getHDI() {
+
+  }
+
+  countLifeExpectancyIndex() {
+
+  }
+
+  countEducationIndex() {
+
+  }
+
+  countIncomeIndex() {
+
+  }
+
 }
